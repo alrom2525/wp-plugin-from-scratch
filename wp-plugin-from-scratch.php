d2d20da0d617a5a105c33ab6ce89608597335a20
alrom2525 <alrom2525@gmail.com>
2022-12-18 17:13:52 -0500

    Part 1

--- wp-plugin-from-scratch.php
+++ wp-plugin-from-scratch.php
@@ -2,3 +2,31 @@
 /**
  * @package Plugin-from-scratch
  */
+/*
+Plugin Name: Plugin from scratch
+Plugin URI: https://github.com/alrom2525/wp-plugin-from-scratch
+Description: This is a plugin write from scratch with educational purposes, The code is explained by <strong>Alessandro Castellani</strong> in his YouTube channel.
+Version: 1.0.0
+Requires PHP: 8.0
+Author: Alessandro Castellani
+Author URI: https://youtu.be/0l7JTie_6jM
+License: GPLv2 or later
+*/
+
+/*
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+Copyright 2005-2022 Automattic, Inc.
+*/
