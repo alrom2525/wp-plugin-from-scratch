43c84a95a49d845d58435d6aa7378f9a82e0b851
alrom2525 <alrom2525@gmail.com>
2022-12-21 20:09:25 -0500

    Part 6 - Enqueue Admin Scripts

--- wp-plugin-from-scratch.php
+++ wp-plugin-from-scratch.php
@@ -41,6 +41,10 @@ class WpPluginFromScratch {
     function __construct() {
         add_action( 'init', array( $this, 'custom_post_type' ) );
     }
+    
+    function register() {
+        add_action( 'admin_enqueue_scripts', array( $this, 'enqueue') );
+    }
 
     function activate() {
         // generated a CPT
@@ -57,10 +61,16 @@ class WpPluginFromScratch {
         register_post_type( 'book', ['public' => true, 'label' => 'Books'] );
     }
     
+    function enqueue () {
+        // enqueue all our scripts
+        wp_enqueue_style('mypluginstyle', plugins_url( '/assets/mystyle.css', __FILE__ ) );
+        wp_enqueue_script('mypluginscript', plugins_url( '/assets/myscript.js', __FILE__ ) );
+    }
 }
 
 if ( class_exists('WpPluginFromScratch') ) {
-    $wpPluginFromScratch = new WpPluginFromScratch();    
+    $wpPluginFromScratch = new WpPluginFromScratch();
+    $wpPluginFromScratch->register();
 }
 
 // activation
